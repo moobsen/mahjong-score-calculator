60bb6aa83aacbcb335be843ec0bdbf5454048976
Moritz Obermeier <mo@bluebirdmountain.de>
2022-10-01 23:14:42 +0200

    adding android stuff

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="@capacitor/local-notifications" />
+/// <reference types="@capacitor/push-notifications" />
+/// <reference types="@capacitor/splash-screen" />
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.moritzobermeier.mahjongcalc',
+  appName: 'mahjong-calc',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+    },
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#CE0B7C',
+    },
+    PushNotifications: {
+      presentationOptions: ['alert', 'sound'],
+    },
+  },
+};
+
+export default config;
