c75083cafbcd8a1fe6cc21f2042828a5feae23f0
Moritz Obermeier <2733691+moobsen@users.noreply.github.com>
2023-11-10 14:27:30 +0100

    increasing android version

--- android/app/src/main/java/de/moritzobermeier/mahjongcalc/MainActivity.java
+++ android/app/src/main/java/de/moritzobermeier/mahjongcalc/MainActivity.java
@@ -13,9 +13,12 @@ public class MainActivity extends BridgeActivity {
     super.onCreate(savedInstanceState);
 
     // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-    }});
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {
+    });
 
+  }
+
+  private void init(Bundle savedInstanceState, ArrayList<Class<? extends Plugin>> classes) {
+  }
 }
+
